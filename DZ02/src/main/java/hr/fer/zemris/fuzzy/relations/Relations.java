101cae58aa168f40dc1e170dcc1ec6397a015f8f
Martin Cekada <cekadamartin@gmail.com>
2019-10-18 10:01:11 +0200

    Finish second homework

--- DZ02/src/main/java/hr/fer/zemris/fuzzy/relations/Relations.java
+++ DZ02/src/main/java/hr/fer/zemris/fuzzy/relations/Relations.java
@@ -3,6 +3,8 @@ package hr.fer.zemris.fuzzy.relations;
 import hr.fer.zemris.fuzzy.domain.CompositeDomain;
 import hr.fer.zemris.fuzzy.domain.DomainElement;
 import hr.fer.zemris.fuzzy.domain.IDomain;
+import hr.fer.zemris.fuzzy.operations.IBinaryFunction;
+import hr.fer.zemris.fuzzy.operations.Operations;
 import hr.fer.zemris.fuzzy.sets.IFuzzySet;
 import hr.fer.zemris.fuzzy.sets.MutableFuzzySet;
 
@@ -61,26 +63,36 @@ public class Relations {
             throw new IllegalArgumentException("To create composition, define relations as U x V and V x W");
         }
 
-        IFuzzySet composition = new MutableFuzzySet(
+        MutableFuzzySet composition = new MutableFuzzySet(
                 CompositeDomain.combine(first.getDomain().getComponent(0), second.getDomain().getComponent(1))
         );
+
+        IBinaryFunction tNorm = Operations.zadehAnd();
+        IBinaryFunction sNorm = Operations.zadehOr();
 
         IDomain joinComposition = first.getDomain().getComponent(1);
         for (DomainElement e : composition.getDomain()) {
             int x = e.getComponentValue(0);
             int z = e.getComponentValue(1);
 
+            double result = -Double.MAX_VALUE;
+
             for (DomainElement y : joinComposition) {
-                
+                DomainElement xy = DomainElement.of(x, y.getComponentValue(0));
+                DomainElement yz = DomainElement.of(y.getComponentValue(0), z);
+                double min = tNorm.valueAt(first.getValueAt(xy), second.getValueAt(yz));
+                result = sNorm.valueAt(result, min);
             }
+
+            composition.set(e, result);
         }
 
 
-        return null;
+        return composition;
     }
 
     public static boolean isFuzzyEquivalence(IFuzzySet set) {
-        return false;
+        return isReflexive(set) && isSymmetric(set) && isMaxMinTransitive(set);
     }
 
     public static boolean isUtimesURelation(IFuzzySet set) {
